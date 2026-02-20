ad6b817cf4cf798f83aaec355d7f991eb2c132c4
tohurtv <joshwebb84@outlook.com>
2026-02-20 14:43:04 -0700

    ....

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ FEDORA_VERSION="${FEDORA_VERSION:-43}"
 # dnf5 -y copr disable ublue-os/staging
 
 #fix Fonts
-rm -f /usr/share/fonts
+rm -rf /usr/share/fonts
 mkdir -p /usr/share/fonts
 ## Install packages
 dnf5 -y install @fonts @hardware-support \
