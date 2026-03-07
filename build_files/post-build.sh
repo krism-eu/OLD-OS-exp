b7ee0cbfe22f850ae5a8156cc0e69ff304bcbfbb
tohurtv <joshwebb84@outlook.com>
2026-03-06 19:57:22 -0700

    add new post-build script

---
+++ build_files/post-build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Rename real dnf5 and dnf binaries
+mv /usr/bin/dnf5 /usr/bin/dnf5.real
+mv /usr/bin/dnf /usr/bin/dnf.real
+
+# Create dnf5 wrapper
+cat > /usr/bin/dnf5 << 'WRAPPER'
+#!/usr/bin/env bash
+COMMAND="${1:-}"
+case "$COMMAND" in
+    install)
+        shift
+        exec rakuos install "$@"
+        ;;
+    remove|erase)
+        shift
+        exec rakuos remove "$@"
+        ;;
+    *)
+        exec /usr/bin/dnf5.real "$@"
+        ;;
+esac
+WRAPPER
+
+# Create dnf wrapper
+cat > /usr/bin/dnf << 'WRAPPER'
+#!/usr/bin/env bash
+exec /usr/bin/dnf5 "$@"
+WRAPPER
+
+# Make both executable
+chmod +x /usr/bin/dnf5 /usr/bin/dnf
+
+echo "RakuOS dnf/dnf5 wrappers installed successfully."
