4fbf1f99a891918c425ab6529e4bb032986ba81a
tohurtv <joshwebb84@outlook.com>
2026-04-12 02:24:53 -0600

    use anaconda-live and libblockdev packages for live environment

--- config/configure-iso.sh
+++ config/configure-iso.sh
@@ -1,5 +1,5 @@
 # Install packages for installation
-dnf5 install -y libblockdev-btrfs
+dnf5 install -y anaconda-live libblockdev-{btrfs,lvm,dm}
 
 #remove rakuos-welcome automatic launch for live environment
 rm -f /etc/xdg/autostart/rakuos-welcome.desktop
